e609ef7e8db11467e919757a8a2567a37b1ff58b
Ariel Basabe <lazurus.kilix@gmail.com>
2023-11-07 18:51:10 -0300

    MODIFICACION ESTRUCTURA NavBar

--- src/app/components/navBar/NavBar.jsx
+++ src/app/components/navBar/NavBar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav>
+      <ul>
+        <li>
+          <Link href="#section-1">
+            Seccion 1
+          </Link>
+        </li>
+        <li>
+          <Link href="#section-2">
+            Seccion 2
+          </Link>
+        </li>
+        <li>
+          <Link href="#section-3">
+            Seccion 3
+          </Link>
+        </li>
+        <li>
+          <Link href="#section-4">
+            Seccion 4
+          </Link>
+        </li>
+        <li>
+          <Link href="#section-5">
+            Seccion 5
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
