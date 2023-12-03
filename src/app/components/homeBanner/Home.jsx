175fba21208880eecff5db0a15794e962905ed65
exequiel <exequielagustindefreitas@gmail.com>
2023-12-02 21:36:42 -0300

    estructura de carousel

--- src/app/components/homeBanner/Home.jsx
+++ src/app/components/homeBanner/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client"; 
+import { useState } from "react"
+import Image from "next/image"
 
 function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const totalImages = 7; 
+
+  function handleNext() {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
+  }
+  
+  function handlePrev() {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
+  }
+
   return (
-    <div id='section-1'>home</div>
-  )
+    <div id='section-1'>
+      <div className='flex items-center justify-between'>
+        <button onClick={handlePrev}>Anterior</button>
+        <Image
+          src={`/images/img${currentIndex + 1}.jpg`}
+          width={200}
+          height={81}
+          alt={`imagen-${currentIndex + 1}`}
+        />
+        <button onClick={handleNext}>Siguiente</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
