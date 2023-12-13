b987781d6ea00ab7bded05c2b5885b475488b579
Ariel Basabe <lazurus.kilix@gmail.com>
2023-12-12 23:08:29 -0300

    Se agrego la galeria de proyectos

---
+++ src/app/components/projects/Projects.jsx
@@ -0,0 +1,121 @@
+"use client"
+
+
+// Components/Gallery.js
+import React, { useState } from "react";
+import { AiOutlineExpandAlt } from "react-icons/ai";
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
+
+const Project = () => {
+  const [open, setOpen] = useState(false);
+  const [image, setImage] = useState("");
+
+  const ProjectB = [
+// you can add more image if you want
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-1.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-2.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-3.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-4.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+    {
+      imageUrl: "https://themewagon.github.io/snapshot/images/model-5.jpg",
+      type: "Nature",
+      title: "Beautiful Work",
+    },
+  ];
+
+
+
+  const slides = ProjectB.map((item) => ({
+    src: item.imageUrl,
+    width: 3840,
+    height: 2560,
+    srcSet: [
+      { src: item.imageUrl, width: 320, height: 213 },
+      { src: item.imageUrl, width: 640, height: 426 },
+      { src: item.imageUrl, width: 1200, height: 800 },
+      { src: item.imageUrl, width: 2048, height: 1365 },
+      { src: item.imageUrl, width: 3840, height: 2560 },
+    ],
+  }));
+
+  return (
+    <div className="w-full">
+      <div className="">
+        <div className="flex flex-col flex-wrap h-full gap-0 mx-2 md:grid md:grid-cols-3 md:mx-0">
+          {ProjectB.map((x, index) => {
+            return (
+              <div key={index} className="md:h-[50vw] h-screen relative">
+                <div className="h-full group">
+                  <div
+                    className="w-full h-full bg-center bg-no-repeat bg-cover"
+                    style={{ backgroundImage: `url("${x.imageUrl}")` }}
+                  >
+                    <div className="absolute bottom-0 z-10 text-3xl text-white left-2">
+                      <div>{x.type}</div>
+                      <div>{x.title}</div>
+                    </div>
+                  </div>
+                  <div
+                    className="absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out bg-black opacity-0 group-hover:opacity-75"
+                    onClick={() => {
+                      setOpen(true);
+                      setImage(x.imageUrl);
+                    }}
+                  >
+                    <p className="text-white">
+                      <AiOutlineExpandAlt className="w-16 h-16 p-3 text-5xl border rounded-full cursor-pointer bg-neutral-500 hover:bg-white hover:text-black" />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        plugins={[Zoom]}
+        showPrevNext={false}
+        slides={slides}
+      />
+    </div>
+  );
+};
+
+export default Project;
+
+
+
+
+
+/*
+import React from 'react'
+
+function Projects() {
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects
+
+*/
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
+
+
+// app/page.js
+
+
+
+/*
+export default function Home() {
+  return (
+    <div className="w-full my-24">
+      <Project />
+    </div>
+  );
+}
+*/
+
+
+
+
+
+import AboutUs from "./components/about-us/AboutUs";
+import Footer from "./components/footer/Footer";
 import HomeB from "./components/homeBanner/Home";
 import NavBar from "./components/navBar/NavBar";
-
+import Project from "./components/projects/Projects";
+import Services from "./components/services/Services";
 
 
 export default function Home() {
@@ -8,9 +31,21 @@ export default function Home() {
     <main>
 
      <NavBar/>
-     <HomeB/>
      
+     <HomeB/>
+
+     <Services/>  
+     
+     <AboutUs />
+     
+     <div className="w-full my-24">
+      <Project />
+    </div>
+     
+     
+     <Footer />
 
     </main>
   );
 }
+
