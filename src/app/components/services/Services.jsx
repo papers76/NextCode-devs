0c8ac9b70b9832202e57eba14013177bfd6a7742
exequiel <exequielagustindefreitas@gmail.com>
2023-12-16 23:04:48 -0300

    componente card:estructura

--- src/app/components/services/Services.jsx
+++ src/app/components/services/Services.jsx
@@ -1,11 +1,81 @@
-
-
-import React from 'react'
+import React from "react";
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <div class="flex min-h-screen items-center justify-center bg-neutral-800">
+      <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+          <div class="h-96 w-72">
+            <img
+              class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+              src="https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920_640.jpg"
+              alt=""
+            />
+          </div>
+          <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+          <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <h1 class="font-dmserif text-3xl font-bold text-white">Desarrollo web</h1>
+            <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Ofrecemos el desarrollo desde landing page hasta sitios web empresariales o autoministrables
+            </p>
+            <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              See More
+            </button>
+          </div>
+        </div>
+        <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+          <div class="h-96 w-72">
+            <img
+              class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+              src="https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305_640.jpg"
+              alt=""
+            />
+          </div>
+          <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+          <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <h1 class="font-dmserif text-3xl font-bold text-white">
+              Aplicaciones de escritorio y moviles
+            </h1>
+            <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              Descripcion acorde a este servicio
+            </p>
+            <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              See More
+            </button>
+          </div>
+        </div>
+        <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+          <div class="h-96 w-72">
+            <img
+              class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+              src="https://cdn.pixabay.com/photo/2017/07/31/11/46/laptop-2557586_1280.jpg"
+              alt=""
+            />
+          </div>
+          <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+          <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <h1 class="font-dmserif text-3xl font-bold text-white">
+              Hosting
+            </h1>
+            <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              No solo creamos tu sitio web o app,sino que nos encargamos de sacarla al mercado y mantenerla
+            </p>
+            <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
+              See More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="fixed bottom-16">
+        <p class="font-com text-2xl font-semibold text-white">
+          All Images are from{" "}
+          <a href="https://unsplash.com" class="text-blue-500">
+            Unsplash.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
