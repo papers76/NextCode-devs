3d1098f59315a8393b5c63adeca621af3db5ec96
exequiel <exequielagustindefreitas@gmail.com>
2023-12-18 19:16:34 -0300

    cambios formato de Home

--- src/app/components/about-us/AboutUs.jsx
+++ src/app/components/about-us/AboutUs.jsx
@@ -1,11 +1,31 @@
+// components/AboutUs.js
+import React from 'react';
 
-
-import React from 'react'
-
-function AboutUs() {
+const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="p-8 bg-gray-200">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="mb-4 text-4xl font-bold text-gray-800">Sobre Nosotros</h1>
+        <p className="text-gray-600">Bienvenido a [Nombre de Tu Empresa], tu socio confiable en el mundo de la programación y desarrollo web.</p>
+        
+        {/* Agrega más contenido según sea necesario */}
+        
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        <p className="text-gray-600">¡Contáctanos hoy y descubre cómo podemos hacer realidad tus visiones tecnológicas!</p>
+        
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- src/app/components/homeBanner/Home.jsx
+++ src/app/components/homeBanner/Home.jsx
@@ -1,39 +1,32 @@
-"use client";
-import { useState } from "react";
+import React from 'react';
+import classNames from 'classnames';
 import styles from './Home.module.css';
 
 function Home() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const totalImages = 7;
-
-  function handleNext() {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
-  }
-
-  function handlePrev() {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
-  }
-
   return (
-    <div id='section-1' className={styles.container}>
-      <div className='flex items-center justify-between'>
-        <button onClick={handlePrev} className={`${styles.navigationButton} bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center`}>
-          &#129168; 
-        </button> 
-        <div className={`${styles.imageContainer} relative w-full h-0 pb-[40%] overflow-hidden transition-opacity duration-500 ease-in-out`}>
-          <img
-            src={`/images/img${currentIndex + 1}.jpg`}
-            alt={`imagen-${currentIndex + 1}`}
-            className={`${styles.image} absolute inset-0 w-full h-full object-cover`}
-          />
-        </div>
-        <button onClick={handleNext} className={`${styles.navigationButton} bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center`}>
-          &#129170;
-        </button>
+    <div
+      className={classNames(
+        'flex items-center justify-center h-screen bg-cover bg-center relative',
+        'bg-opacity-70',
+        styles.darkImage 
+      )}
+    >
+      <div className="text-center text-white">
+        <h1 className="text-4xl font-bold mb-4">Nexo Desarrollo</h1>
+        <p className="text-lg mb-8">
+          Tu socio en desarrollo web, aplicaciones móviles y aplicaciones de escritorio.
+        </p>
+        <a
+          href="#contact"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300"
+        >
+          ¡Contáctanos!
+        </a>
       </div>
     </div>
   );
 }
 
 export default Home;
+
 
